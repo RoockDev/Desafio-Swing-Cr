9ac96eb16ace3351fab815cd60a4ad727b1c12b7
Sergio Zarcero Perez <zar0_31@outlook.com>
2025-10-13 01:02:42 +0200

    refactor(datos): Diferenciar tipos de evento en los datos
    Se añade la propiedad "tipo" a cada objeto de actividad para permitir la creación de diferentes tipos de instancias (Clase o Actividad).
    
    Se estandarizan también otros nombres de propiedades para que coincidan con los constructores de las clases.

---
+++ src/modules/actividadesModales.js
@@ -0,0 +1,72 @@
+export {actividades};
+
+        const actividades = [
+    {
+        id: 'lindy-hop',
+        tipo: 'clase',
+        nombre: 'Lindy-Hop',
+        dia: 'Sabado 11',
+        hora: 'Todo el dia',
+        ubicacion: 'Sala Be Hopper',
+        nivel: 'Basico',
+        profesores: ['Hyunjung', 'Anders'],
+        descripcion: 'Aprende los pasos básicos del Lindy Hop con nuestros profesores internacionales'
+    },
+    {
+        id: 'solo jazz',
+        tipo: 'clase',
+        nombre: 'Solo jazz',
+        dia: 'Sabado 11',
+        hora: 'Todo el dia',
+        ubicacion: 'Sala savoy',
+        nivel: 'Intermedio',
+        profesores: ['Swinghappy'],
+        descripcion: 'Disfruta del mejor jazz al estilo New Orleans, Dont worry, be Swinghappy'
+    },
+    {
+        id:'tastet con Marta y Pedro',
+        tipo: 'clase',
+        nombre: 'Taster con Marta y Pedro',
+        dia: 'Viernes 10',
+        hora: '20:00-21:00',
+        ubicacion: 'Sala Be Hopper',
+        nivel: 'Multinivel',
+        profesores: ['Marta','Pedro'],
+        descripcion: 'Lo mejor del Swing de los pies de Marta y Pedro, no te lo pierdas¡'
+
+    },
+    {
+        id:'baile social',
+        tipo: 'actividad',
+        nombre: 'Baile Social',
+        dia: 'Sabado 11',
+        hora: '12:30-17:00',
+        ubicacion: 'Parque de Gasset',
+        nivel: 'Multinivel',
+        profesores: [],
+        descripcion: 'Ven a disfrutar del swing al parque gasset donde podras bailar durante inumerables horas'
+    },
+    {
+        id:'concierto fin de festival',
+        tipo: 'actividad',
+        nombre: 'Concierto fin de festival',
+        dia: 'Domingo 12',
+        hora: '16:30-20:00',
+        ubicacion: 'Antiguo casino',
+        nivel: 'Multinivel',
+        profesores: [],
+        descripcion: 'Música, Música y más Música!! Sabemos que duelen los pies, pero dales un último regalo y ven al concierto final¡'
+    },
+    {
+        id: 'taster con Dani y Ross',
+        tipo: 'clase',
+        nombre: 'Taster con Dani y Ross',
+        dia: 'Sabado 11',
+        hora: '21:00-22:00',
+        ubicacion: 'Sala New Orleans',
+        nivel: 'Avanzado',
+        profesores: ['Dani','Ross'],
+        descripcion: 'Solo los más valientes están dispuesto a bailer al ritmo de Dani y Ross, si eres uno de esos valientes no te lo puedes perder'
+
+    }
+];
