346a6be9a9fb72f52d18308d8174965b77fbcfe0
Sergio Zarcero Perez <zar0_31@outlook.com>
2025-10-12 19:30:27 +0200

    feat: Integrar modulos en main.js
    Se integran los modulos creados anteriormente al archivo main.js

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import './assets/css/style.css';
+import { initArtistCards } from './modules/tarjetasArtistas';
+import { initModalVideo } from './modules/modalVideo';
+
+initArtistCards();
+initModalVideo();
