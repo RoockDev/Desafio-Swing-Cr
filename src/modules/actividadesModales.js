2b95a61bb45e2310a2d103f6488e9fe26acb58d3
Sergio Zarcero Perez <zar0_31@outlook.com>
2025-10-13 22:23:18 +0200

    feat(data): Completar y refactorizar la lista de eventos
    Se actualiza el array de actividades para que sea un reflejo completo del horario del festival.
    
    - Se añaden las actividades que faltaban.
    - Se refactoriza la propiedad 'hora' de las clases para que sea un array de franjas horarias.

--- src/modules/actividadesModales.js
+++ src/modules/actividadesModales.js
@@ -1,72 +1,178 @@
-export {actividades};
 
-        const actividades = [
-    {
-        id: 'lindy-hop',
-        tipo: 'clase',
-        nombre: 'Lindy-Hop',
-        dia: 'Sabado 11',
-        hora: 'Todo el dia',
-        ubicacion: 'Sala Be Hopper',
-        nivel: 'Basico',
-        profesores: ['Hyunjung', 'Anders'],
-        descripcion: 'Aprende los pasos básicos del Lindy Hop con nuestros profesores internacionales'
-    },
-    {
-        id: 'solo jazz',
-        tipo: 'clase',
-        nombre: 'Solo jazz',
-        dia: 'Sabado 11',
-        hora: 'Todo el dia',
-        ubicacion: 'Sala savoy',
-        nivel: 'Intermedio',
-        profesores: ['Swinghappy'],
-        descripcion: 'Disfruta del mejor jazz al estilo New Orleans, Dont worry, be Swinghappy'
-    },
-    {
-        id:'taster con Marta y Pedro',
-        tipo: 'clase',
-        nombre: 'Taster con Marta y Pedro',
-        dia: 'Viernes 10',
-        hora: '20:00-21:00',
-        ubicacion: 'Sala Be Hopper',
-        nivel: 'Multinivel',
-        profesores: ['Marta','Pedro'],
-        descripcion: 'Lo mejor del Swing de los pies de Marta y Pedro, no te lo pierdas¡'
 
-    },
-    {
-        id:'baile social',
-        tipo: 'actividad',
-        nombre: 'Baile Social',
-        dia: 'Sabado 11',
-        hora: '12:30-17:00',
-        ubicacion: 'Parque de Gasset',
-        nivel: 'Multinivel',
-        profesores: [],
-        descripcion: 'Ven a disfrutar del swing al parque gasset donde podras bailar durante inumerables horas'
-    },
-    {
-        id:'concierto fin de festival',
-        tipo: 'actividad',
-        nombre: 'Concierto fin de festival',
-        dia: 'Domingo 12',
-        hora: '16:30-20:00',
-        ubicacion: 'Antiguo casino',
-        nivel: 'Multinivel',
-        profesores: [],
-        descripcion: 'Música, Música y más Música!! Sabemos que duelen los pies, pero dales un último regalo y ven al concierto final¡'
-    },
-    {
-        id: 'taster con Dani y Ross',
-        tipo: 'clase',
-        nombre: 'Taster con Dani y Ross',
-        dia: 'Sabado 11',
-        hora: '21:00-22:00',
-        ubicacion: 'Sala New Orleans',
-        nivel: 'Avanzado',
-        profesores: ['Dani','Ross'],
-        descripcion: 'Solo los más valientes están dispuesto a bailer al ritmo de Dani y Ross, si eres uno de esos valientes no te lo puedes perder'
-
-    }
+export const actividades = [
+  {
+    id: "lindy-hop-sabado(Anders & Hyunjung)",
+    tipo: "clase",
+    nombre: "Lindy-Hop(Anders & Hyunjung)",
+    dia: "Sabado",
+    hora: [
+        '10:30 - 11:30',
+    '11:35 - 12:35',
+    '12:40 - 13:40',
+    '17:00 - 18:00',
+    '18:10 - 19:10'
+    ],
+    ubicacion: "Sala Be Hopper",
+    nivel: "Basico",
+    profesores: ["Hyunjung", "Anders"],
+    descripcion:
+      "Aprende los pasos básicos del Lindy Hop con nuestros profesores internacionales",
+  },
+  {
+    id: "lindy-hop-domingo(Anders & Hyunjung)",
+    tipo: "clase",
+    nombre: "Lindy-Hop(Anders & Hyunjung)",
+    dia: "Domingo",
+    hora: [
+        '10:00 - 11:00',
+    '11:05 - 12:05',
+    '12:10 - 13:10'
+    ],
+    ubicacion: "Sala Be Hopper",
+    nivel: "Basico",
+    profesores: ["Hyunjung", "Anders"],
+    descripcion:
+      "Aprende los pasos básicos del Lindy Hop con nuestros profesores internacionales",
+  },
+  {
+    id: "lindy-hop-sabado(Marta & Pedro)",
+    tipo: "clase",
+    nombre: "Lindy-Hop(Marta & Pedro)",
+    dia: "Sabado",
+    hora: [
+        '10:30 - 11:30',
+    '11:35 - 12:35',
+    '12:40 - 13:40',
+    '17:00 - 18:00',
+    '18:10 - 19:10'
+    ],
+    ubicacion: "Sala New Orleans",
+    nivel: "Intermedio",
+    profesores: ["Marta", "Pedro"],
+    descripcion:
+      "Aprende los pasos básicos del Lindy Hop con nuestros profesores internacionales",
+  },
+  {
+    id: "lindy-hop-domingo(Marta & Pedro)",
+    tipo: "clase",
+    nombre: "Lindy-Hop(Marta & Pedro)",
+    dia: "Domingo",
+    hora: [
+        '10:00 - 11:00',
+    '11:05 - 12:05',
+    '12:10 - 13:10'
+    ],
+    ubicacion: "Sala New Orleans",
+    nivel: "Intermedio",
+    profesores: ["Marta", "Pedro"],
+    descripcion:
+      "Aprende los pasos básicos del Lindy Hop con nuestros profesores internacionales",
+  },
+  {
+    id: "solo jazz-sabado",
+    tipo: "clase",
+    nombre: "Solo jazz",
+    dia: "Sabado",
+    hora: [
+        '10:30 - 11:30',
+    '11:35 - 12:35',
+    '12:40 - 13:40',
+    '17:00 - 18:00',
+    '18:10 - 19:10'
+    ],
+    ubicacion: "Sala savoy",
+    nivel: "Intermedio",
+    profesores: ["Swinghappy"],
+    descripcion:
+      "Disfruta del mejor jazz al estilo New Orleans, Dont worry, be Swinghappy",
+  },
+   {
+    id: "solo jazz-domingo",
+    tipo: "clase",
+    nombre: "Solo jazz",
+    dia: "Domingo",
+    hora: [
+        '10:00 - 11:00',
+    '11:05 - 12:05',
+    '12:10 - 13:10'
+    ],
+    ubicacion: "Sala savoy",
+    nivel: "Intermedio",
+    profesores: ["Swinghappy"],
+    descripcion:
+      "Disfruta del mejor jazz al estilo New Orleans, Dont worry, be Swinghappy",
+  },
+  {
+    id: "taster con Marta y Pedro",
+    tipo: "clase",
+    nombre: "Taster con Marta y Pedro",
+    dia: "Viernes",
+    hora: "20:00 - 21:00",
+    ubicacion: "Sala Be Hopper",
+    nivel: "Multinivel",
+    profesores: ["Marta", "Pedro"],
+    descripcion:
+      "Lo mejor del Swing de los pies de Marta y Pedro, no te lo pierdas¡",
+  },
+  {
+    id: "baile social",
+    tipo: "actividad",
+    nombre: "Baile Social",
+    dia: "Sabado",
+    hora: "12:30 - 17:00",
+    ubicacion: "Parque de Gasset",
+    nivel: "Multinivel",
+    profesores: [],
+    descripcion:
+      "Ven a disfrutar del swing al parque gasset donde podras bailar durante inumerables horas",
+  },
+  {
+    id: "concierto fin de festival",
+    tipo: "actividad",
+    nombre: "Concierto fin de festival",
+    dia: "Domingo",
+    hora: "16:30 - 20:00",
+    ubicacion: "Antiguo casino",
+    nivel: "Multinivel",
+    profesores: [],
+    descripcion:
+      "Música, Música y más Música!! Sabemos que duelen los pies, pero dales un último regalo y ven al concierto final¡",
+  },
+  {
+    id: "taster con Dani y Ross",
+    tipo: "clase",
+    nombre: "Taster con Dani y Ross",
+    dia: "Sabado",
+    hora: "21:00 - 22:00",
+    ubicacion: "Sala New Orleans",
+    nivel: "Avanzado",
+    profesores: ["Dani", "Ross"],
+    descripcion:
+      "Solo los más valientes están dispuesto a bailer al ritmo de Dani y Ross, si eres uno de esos valientes no te lo puedes perder",
+  },
+  {
+    id: "preliminares-mix-match",
+    tipo: "actividad",
+    nombre: "Preliminares Mix & Match (Open y Switch)",
+    dia: "Viernes",
+    hora: "21:15 - 22:00",
+    ubicacion: "Sala Be Hopper + Sala New Orleans",
+    nivel: "Multinivel",
+    profesores: [],
+    descripcion:
+      "Competición de baile por parejas. ¡Anímate a participar o a disfrutar del espectáculo!",
+  },
+  {
+    id: "fiesta-viernes",
+    tipo: "actividad",
+    nombre: "Fiesta con Carolina Reapers Swing",
+    dia: "Viernes",
+    hora: "22:30 - 3:30",
+    ubicacion: "Sala Be Hopper + Sala New Orleans",
+    nivel: "Todos los niveles",
+    profesores: [],
+    descripcion:
+      "¡La primera gran fiesta del festival con música en directo a cargo de Carolina Reapers Swing!",
+  }
 ];
