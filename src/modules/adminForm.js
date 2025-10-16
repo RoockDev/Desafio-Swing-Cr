83ce20ad58d2cc869e9a51f1df0915d60f4c4800
Sergio Zarcero Perez <zar0_31@outlook.com>
2025-10-16 19:00:46 +0200

    feat: Añadir validacion de duración y tramos horarios
    Se añade logica para que los eventos no puedan tener una duracion inferior a 30 minutos y para que la eleccion de horas solo puedan ser incremento de 5 minutos

--- src/modules/adminForm.js
+++ src/modules/adminForm.js
@@ -22,11 +22,20 @@ if (evento.value === 'clase') {
 
 tipoEvento();
 
+const limpiarMensajes = () => {
+    const mensajeFormulario = document.getElementById('form-message');
+    mensajeFormulario.classList.remove('form-message--error','form-message-success');
+
+    mensajeFormulario.textContent = '';
+};
+
 const adminForm = document.querySelector('.admin-form');
 
 const handleFormSubmit = (e) => {
     //evitamos que la pagina se recargue
     e.preventDefault();
+    limpiarMensajes();
+
     let eventosGuardados = localStorage.getItem('eventos');
     let listaEventos;
     if (eventosGuardados === null) {
@@ -35,17 +44,61 @@ const handleFormSubmit = (e) => {
         listaEventos = JSON.parse(eventosGuardados);
     };
 
+    
      const dia = document.getElementById('evento-dia').value;
-     const hora = document.getElementById('evento-hora').value;
+     const horaInicio = document.getElementById('evento-hora').value;
+     const horaFin = document.getElementById('evento-fin').value;
      const tipo = document.getElementById('tipo-evento').value;
      const estilo = document.getElementById('clase-estilo').value;
      const ubicacion = document.getElementById('evento-ubicacion').value;
      const tieneProfesores = document.querySelector('input[name="tiene-profesores"]:checked').value;
+    const mensajeFormulario = document.getElementById('form-message'); 
+
+    //queremos que los minutos de las clases para la hora inicio y fin puedan ser incrementos de 5
+    const minutosInicio = parseInt(horaInicio.split(':')[1]);
+    const minutosFin = parseInt(horaFin.split(':')[1]);
+
+    if (minutosInicio % 5 !== 0 || minutosFin % 5 !== 0) {
+        mensajeFormulario.classList.add('form-message--error');
+        mensajeFormulario.textContent = 'Las horas deben ser en incrementos de 5 minutos (ej: 10:05, 10:10) ';
+        return;
+    }else{
+        limpiarMensajes();
+    };
+
+    //ahora validamos por que queremos que los eventos tengan una duracion minima de 30 min
+    //necesitamos obtener los minutos totales
+    const [hInicio,mInicio] = horaInicio.split(':');
+    const totalMinutosInicio = parseInt(hInicio) * 60 + parseInt(mInicio);
+
+    const [hFin,mFin] = horaFin.split(':');
+    const totalMinutosFin = parseInt(hFin) * 60 + parseInt(mFin);
+
+    const duracion = totalMinutosFin - totalMinutosInicio;
+    if (duracion <30 ) {
+        mensajeFormulario.classList.add('form-message--error');
+        mensajeFormulario.textContent = 'La duracion minima debe ser de 30 minutos';
+        
+        return;
+    }else{
+        limpiarMensajes();
+    };
     
+
+
+     if (horaFin <= horaInicio) {
+        mensajeFormulario.classList.add('form-message--error');
+        mensajeFormulario.textContent = 'Horario incorrecto para registrar, la hora fin debe ser posterior al inicio';
+        return;
+     }else{
+        limpiarMensajes();
+        
+     };
 
      const datosEvento = {
         dia: dia,
-        hora: hora,
+        horaInicio: horaInicio,
+        horaFin: horaFin,
         tipo: tipo,
         tieneProfesores: tieneProfesores,
         estilo: estilo,
