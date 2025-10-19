765b8e256c59f93720d8137740519e1276a95cbc
Sergio Zarcero Perez <zar0_31@outlook.com>
2025-10-20 01:02:08 +0200

    fix: Solucionar problema con los atributos del objeto padre Evento

--- src/models/Evento.js
+++ src/models/Evento.js
@@ -1,10 +1,11 @@
 export class Evento{
-    constructor({id,nombre,dia,horaInicio,horaFin,duracionHoras,ubicacion,estilo,descripcion}){
+    constructor({id,nombre,dia,horaInicio,ubicacion,estilo,descripcion}){
         this.id = id;
         this.dia = dia;
         this.horaInicio = horaInicio;
-        this.horaFin = horaFin;
-        this.duracionHoras =duracionHoras
+        // Calcular automáticamente horaFin (siempre +1 hora)
+        const hora = parseInt(horaInicio.split(':')[0], 10);
+        this.horaFin = `${String((hora + 1) % 24).padStart(2, '0')}:00`;
         this.ubicacion = ubicacion;
         this.estilo = estilo;
     }
